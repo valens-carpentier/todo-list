0ae1bed88d5fee180abd294a3e616c8121212036
zalenscarpentier <168520191+zalenscarpentier@users.noreply.github.com>
2024-09-09 08:56:33 +0200

    inital skeleton and html skeleton

---
+++ src/addTask.js
@@ -0,0 +1,4 @@
+// when clicing on the add task button, a new task input field should appear
+// the user should be able to add a task name, priority, and due date
+// the user should be able to submit the task
+// the task should be added to the list of tasks    
