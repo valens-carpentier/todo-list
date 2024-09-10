4cbd239266e2e5d0a26670ab5665ca1c8b34a3ce
zalenscarpentier <168520191+zalenscarpentier@users.noreply.github.com>
2024-09-10 20:31:34 +0200

    challenge completed

---
+++ ec64c2d2298fdacb14c3.js
@@ -0,0 +1,17 @@
+// index.js
+import { addTasktoLibrary } from "./addTask.js";
+import { displayTasks } from "./displayTasks.js";
+import { addTaskModal } from "./addTaskModal.js";
+import { filterCategory } from "./filterCategory.js";
+import { expandModal } from "./expandModal.js";
+import { filterDate } from "./filterDate.js";
+import "./styles.css";
+
+// Initial display of tasks
+displayTasks();
+addTaskModal();
+addTasktoLibrary();
+filterCategory();
+expandModal();
+filterDate();
+
